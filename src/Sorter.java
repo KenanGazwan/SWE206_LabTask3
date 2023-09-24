cebb9ceef9328a1be93ed76e3a76e92638e0f454
KenaGazwan <kenangazwan@gmail.com>
2023-09-24 20:03:54 +0300

    Comment ADDED

--- src/Sorter.java
+++ src/Sorter.java
@@ -3,13 +3,14 @@ public class Sorter {
         System.out.println("Hello, World!");
     }
 
+    // Computer A Method 
     public static void selectionSort(int[] arr){  
         for (int i = 0; i < arr.length - 1; i++)  
         {  
             int index = i;  
             for (int j = i + 1; j < arr.length; j++){  
                 if (arr[j] < arr[index]){  
-                    index = j;//searching for lowest index  
+                    index = j;  //searching for lowest index  
                 }  
             }  
             int smallerNumber = arr[index];   
