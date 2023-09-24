0c11a7210dc99d09d97bb31b2c03d2b29282e106
KenaGazwan <kenangazwan@gmail.com>
2023-09-24 19:52:37 +0300

    Selection Sort Added

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,20 @@ public class Sorter {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }
+
 }
